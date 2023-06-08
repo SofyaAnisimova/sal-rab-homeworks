e8c2aa89cc7a6e82eba485891f889d0af437d98e
SofyaAnisimova <133322456+SofyaAnisimova@users.noreply.github.com>
2023-06-08 22:20:46 +0700

    itog

--- task_final/solution.js
+++ task_final/solution.js
@@ -34,7 +34,7 @@
 // }
 
 function sendRequest(name, phone, address, goods, sum) {
-    let data = {client: {name, phone}, order: {address, sum}, goods: [{title, count}]};
+    let data = {client: {name, phone}, order: {address, sum}, goods: []};
 
     let countOfGoods = goods.length;
 
@@ -42,7 +42,7 @@ function sendRequest(name, phone, address, goods, sum) {
         data.goods.push({title:goods[i].title, count:goods[i].count});
     }
 
-    data.order.address =  "ул. " + address.street + ", дом " + address.house + ", подъезд" + address.entrance + ", этаж " + address.floor + ", кв. " + address.flat;
+    data.order.address =  "ул. " + address.street + ", дом " + address.house + address.entrance + ", подъезд " +  address.floor + ", этаж " +  ", кв. " + address.flat;
     data.order.sum =  sum;
 
     data.client = name + " " + phone;
